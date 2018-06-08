b8ff20299b3324057f0631e0fb07704953eae5f7
Ludim <ludim.anel@gmail.com>
2018-06-08 14:41:09 -0500

    Feat [find_digits_ludim.py]: Add findDigitsDigitalBase

--- reto-1-soluciones/find_digits_ludim.py
+++ reto-1-soluciones/find_digits_ludim.py
@@ -1,11 +1,20 @@
 #!/bin/python3
+DIGITAL_BASE = 10
+
+# To improve code: https://www.hackerrank.com/challenges/find-digits/topics/div-mod
+def findDigitsDigitalBase(n):
+    count = 0
+    while (n != 0):
+        number = n % DIGITAL_BASE
+        if number != 0 and n % number == 0: count += 1
+        n //= 10
+    return count
 
 def findDigits(n):
     numbers = str(n)
     count = 0
     for number in numbers:
-        if int(number) != 0 and n % int(number) == 0:
-            count += 1
+        if int(number) != 0 and n % int(number) == 0: count += 1
     return count
     
 if __name__ == '__main__':
@@ -13,4 +22,5 @@ if __name__ == '__main__':
     for t_itr in range(t):
         n = int(input("> "))
         result = findDigits(n)
+        result = findDigitsDigitalBase(n)
         print(result)
